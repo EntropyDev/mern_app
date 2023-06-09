87622eef6910b140b57015fddce16830637d5e38
Vaibhav Chimalgi <vchimu20@gmail.com>
2023-06-09 21:22:22 +0530

    navbar view added

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,6 +1,11 @@
+import {Box} from "@mui/material"
+import NavBar from "../navbar"
 const HomePage = () => {
     return (
-        <div>HomePage</div>
+        <Box>
+            <NavBar />
+        </Box>
+        // <div>HomePage</div>
     )
 }
 export default HomePage
