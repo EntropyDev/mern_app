d012d0aa55053ce8ba0c9f5b370f445aa3d8a4fd
Vaibhav Chimalgi <vchimu20@gmail.com>
2023-06-10 23:23:12 +0530

    error fix

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -16,14 +16,14 @@ export const register = async (req, res) => {
             occupation
         } = req.body
 
-        const salt = await bcrypt.getSalt()
+        const salt = await bcrypt.genSalt()
         const passwordHash = await bcrypt.hash(password, salt)
 
         const newUser = new User({
             firstName,
             lastName,
             email,
-            passwordHash,
+            password: passwordHash,
             picturePath,
             friends,
             location,
@@ -42,7 +42,7 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
     try{
         const {email,password} = req.body
-        const user = awaitUser.findOne({email: email})
+        const user = await User.findOne({email: email})
         if(!user) return res.status(400).json({msg: "user does not exist"})
 
         const isMatch = await bcrypt.compare(password, user.password)
